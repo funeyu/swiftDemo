eccdaf80a8ff6ed96a6fef6ee49cebe0ff1ce748
funeyu <funeyu@gmail.com>
2017-10-06 18:02:12 +0800

    添加READMe

---
+++ pages/README.md
@@ -0,0 +1,7 @@
+#  简介
+页面间跳转返回
+
+## 知识点
++ 由代码`ArticleViewController.swift运行结果可知,`viewDidLoad`,`viewWillAppear`,`viewDidAppear`三个的执行顺序：
+`viewDidLoad`---->`viewWillAppear` ----> `viewDidAppear`
+
--- pages/pages/ArticleViewController.swift
+++ pages/pages/ArticleViewController.swift
@@ -29,9 +29,33 @@ class ArticleViewController: UIViewController {
         button.center = CGPoint(x: fullSize.width * 0.5, y: fullSize.height * 0.2)
         button.addTarget(self, action: #selector(self.goBack), for: .touchUpInside)
         self.view.addSubview(button)
+        
+        print("viewDidLoad")
     }
     
     @objc func goBack() {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
     }
 }
